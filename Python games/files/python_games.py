9fe034782429375aa01534d65a6bb2a534506bf6
glue0822 <25328242@QQ.com>
2025-02-07 14:06:21 +0800

    Update

--- Python games/files/python_games.py
+++ Python games/files/python_games.py
@@ -1,3 +1,8 @@
 import subprocess
-subprocess.call(['pythonw.exe', 'log_in.py'])
+sign=open(r'data/sign.ini')
+sign.seek(0,0)
+if sign.read() == 'False':
+    subprocess.call(['pythonw.exe', 'log_in.py'])
+if sign.read() == 'True':
+    subprocess.call(['pythonw.exe', 'hall.py'])
 #只是个引导，我怕用python.exe解释的控制台碍事
